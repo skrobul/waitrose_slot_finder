9890d75e3e6e17e014d15e9a04a0564ce86b685f
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2020-03-27 14:21:46 +0000

    include NOTIFIER in start-example

--- start-example.sh
+++ start-example.sh
@@ -10,4 +10,5 @@ docker run \
     -e PUSHOVER_APP_TOKEN=acd6pcunf7bv9z8noe2a63zo12345 \
     -e PUSHOVER_USER_KEY=uw14zr312jhdjahj1 \
     -e CHECK_EVERY_MINUTES=60 \
+    -e NOTIFIER=gotify \
     skrobul/wrose-slot-checker
