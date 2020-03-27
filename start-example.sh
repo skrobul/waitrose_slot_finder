61dc312495fa655eaa97fee68c9d15a0911c5db9
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2020-03-27 14:13:47 +0000

    add examples on how to start this

---
+++ start-example.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+docker run \
+    --name=trose_slot_checker \
+    --restart=always \
+    -d \
+    -e TROSE_USER=example@example.com \
+    -e TROSE_PASSWORD=very_secret \
+    -e GOTIFY_APP_TOKEN=AX1234 \
+    -e GOTIFY_URL=https://example.com/gotify \
+    -e PUSHOVER_APP_TOKEN=acd6pcunf7bv9z8noe2a63zo12345 \
+    -e PUSHOVER_USER_KEY=uw14zr312jhdjahj1 \
+    -e CHECK_EVERY_MINUTES=60 \
+    skrobul/wrose-slot-checker
